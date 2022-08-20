bc86070be46d3634f5edfb5ace736ea7d27dd5e1
fatimah06 <fatima.m.mutairi@gmail.com>
2022-08-20 14:38:06 +0300

    js

--- string.js
+++ string.js
@@ -1,5 +1,17 @@
 var myString = "I Don\'t Get My Dream Job Unit";
 
+var mynumber = 30;
+console.log(mynumber);
+console.log(typeof(mynumber));
+var mynew =  mynumber.toString();
+console.log(mynew);
+console.log(typeof(mynew));
+
+var myWord = myString.indexOf("My")  // اداة بحث عن الكلمه بين القوسين وممكن تحديد من اي رقم انديكس يبدا البحث
+console.log(myWord);
+// ممكن استعمال search نفس الانديكس اوف 
+
+
 var mySplittedString = myString.split(" ");
 // يحول من سترينق الى اوبجكت
 
