1e7668373320e0abed361f305aabc165dba9f455
fatimah06 <fatima.m.mutairi@gmail.com>
2022-08-20 12:08:24 +0300

    js

---
+++ string.js
@@ -0,0 +1,16 @@
+var myString = "I Don\'t Get My Dream Job Unit";
+
+var mySplittedString = myString.split(" ");
+// يحول من سترينق الى اوبجكت
+
+console.log(myString);
+console.log(typeof(myString));
+
+console.log(mySplittedString);
+console.log(typeof(mySplittedString));
+
+var mySlicedString = myString.slice(1);
+// اطلع شريحه من النص لمتغير جديد ممكن استفيد من الجزء اللي اخذته من النص باي طريقه
+
+console.log(mySlicedString);
+console.log(typeof(mySlicedString))
