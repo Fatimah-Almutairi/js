c136a77c5d7fab8e1406eed5df7452a860872c07
fatimah06 <fatima.m.mutairi@gmail.com>
2022-08-20 17:50:28 +0300

    js

--- string.js
+++ string.js
@@ -1,4 +1,4 @@
-var myString = "I Don\'t Get My Dream Job Unit";
+var myString = "I Don 't Get My Dream Job Unit!";
 
 var mynumber = 30;
 console.log(mynumber);
@@ -7,7 +7,7 @@ var mynew =  mynumber.toString();
 console.log(mynew);
 console.log(typeof(mynew));
 
-var myWord = myString.indexOf("My")  // اداة بحث عن الكلمه بين القوسين وممكن تحديد من اي رقم انديكس يبدا البحث
+var myWord = myString.indexOf("My");  // اداة بحث عن الكلمه بين القوسين وممكن تحديد من اي رقم انديكس يبدا البحث
 console.log(myWord);
 // ممكن استعمال search نفس الانديكس اوف 
 
@@ -25,4 +25,13 @@ var mySlicedString = myString.slice(1); // بين القوسين اعلمه من
 // اطلع شريحه من النص لمتغير جديد ممكن استفيد من الجزء اللي اخذته من النص باي طريقه
 
 console.log(mySlicedString);
-console.log(typeof(mySlicedString))
+console.log(typeof(mySlicedString));
+
+var mySub = myString.substring(4,19);
+console.log(mySub);
+
+var myChar = myString.charAt(3);   // يرجع لي الحرف الموجود برقم الانديكس اللي حطيته بين القوسين
+console.log(myChar); 
+
+var myChart = myString.charAt(myString.length - 1);  // بيرجع رقم الانديكس الاخير فبالتالي اخر حرف هو اللي الطول ناقص واحد
+console.log(myChart);
