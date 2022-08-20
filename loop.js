064e3701333737620141b93e6973e80a8f00c79c
fatimah06 <fatima.m.mutairi@gmail.com>
2022-08-20 22:52:49 +0300

    loop

---
+++ loop.js
@@ -0,0 +1,53 @@
+var i = 0;
+while(i <= 10){
+    console.log(i);
+
+    i++;
+}
+///------------------------
+var l = 0;
+do{
+    console.log(l);
+    l++;
+
+} while(l <= 10);
+///------------------------
+
+for(var i = 0; i <= 10; i++){
+    console.log(i);
+
+}
+///------------------------
+
+var myCar = {
+    name: "Acoard",
+    type: "sedan",
+    year: 2019,
+    color: "White"
+};
+
+for(var prop in myCar){
+    if(myCar.hasOwnProperty(prop)){        //   فايدة الايف هنا عشان يطلع الصفات اللي بالاوبجكت هذا بس مايعطيني بروب من برا 
+
+        console.log(prop + ": " + myCar[prop]);
+    }
+}
+
+
+// break & continue 
+// فايدة البريك يوقف اللوب عند شرط معين - والكونتينيو بشرط اذا حصله يطمر النتيجة المساويه للشرط ويكمل اللوب 
+
+function generateYears(Start, End){
+    var years;
+
+    document.write("<select>");
+
+    for(years = Start; years <= End; years++){
+        document.write("<option value=\"" +years + "\">" + years + "</option>");
+
+
+    }
+    document.write("</select>");
+
+}
+generateYears(2010, 2022);
