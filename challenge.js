d823c4b4cefb2718e588bc5e853a84bd07bb7aef
fatimah06 <fatima.m.mutairi@gmail.com>
2022-08-23 14:08:11 +0300

    js

--- challenge.js
+++ challenge.js
@@ -4,4 +4,12 @@ let c = 1e2;
 let d = 2.4;
 
 
-console.log(Math.min(a, b, c, d))
+console.log(Math.floor(Math.min(a, b, c, d)));
+
+console.log(a ** Math.trunc(d));
+
+console.log(Math.floor(d));
+console.log(Math.trunc(d));
+console.log(Number.parseInt(d));
+console.log(Math.round(d));
+
