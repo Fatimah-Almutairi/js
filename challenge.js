55b88ab808aab5880ba16cf6cf5967572b6bf8fc
fatimah06 <fatima.m.mutairi@gmail.com>
2022-08-24 22:56:36 +0300

    html

--- challenge.js
+++ challenge.js
@@ -35,15 +35,19 @@ console.log(ab.charAt(0).toLowerCase() + ab.slice(1, length-1).toUpperCase() + a
 let st = "Elzero Web School";
 
 if ((st.length*2).toString()=== "34") {
+
   console.log("Good");
 }
 
 // W Position May Change
+
 if (st[st.indexOf("W")].toLowerCase() === "w") {
+
   console.log("Good");
 }
 
 if (st.length !== "string") {
+    
   console.log("Good");
 }
 
