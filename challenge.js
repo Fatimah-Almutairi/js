eb2a8e6abbc03e93957ef0da5864da7369ad55ad
fatimah06 <fatima.m.mutairi@gmail.com>
2022-08-26 22:41:56 +0300

    js

--- challenge.js
+++ challenge.js
@@ -89,4 +89,4 @@ console.log(my.splice(zero, ++zero, "rO"));
 // ================================================ LOOP
 
 let myAdmins = ["Ahmad", "Osama", "Sayed", "Stop", "Samera"];
-let myEmploees = []
+let myEmploees = ["Amgad", "Samah", ""]
