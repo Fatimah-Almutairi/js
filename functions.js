66bb8e5ab6c4d987732a6dba1ba7aaf549e0f183
fatimah06 <fatima.m.mutairi@gmail.com>
2022-08-29 10:40:12 +0300

    js

--- functions.js
+++ functions.js
@@ -4,3 +4,16 @@ function calc(num1, num2){
 
 let result = calc(20, 10);
 console.log(result + 100);
+
+// ======================================
+
+function generate(start, end){
+    for (let i = start; i < end; i++){
+        console.log(i);
+        if(i === 15){
+            return 'Interruptting';
+        }
+    }
+}
+
+generate(10, 20);
