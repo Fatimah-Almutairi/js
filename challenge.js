990f2439fcf79671af8badc4b237770d0a96af99
fatimah06 <fatima.m.mutairi@gmail.com>
2022-08-31 15:26:25 +0300

    js

--- challenge.js
+++ challenge.js
@@ -188,7 +188,7 @@ showDetails("Fatimah", false, 26);
 
 // ================================
 
-let names =  (...nam) =>{ return `String [${nam.join(", ")}] => Done!`}
+let names =  (...nam) =>{ return `String [${nam.join("] , [")}] => Done!`}
 
 
 console.log(names("Fatimah", "Bassam", "Youssef"));
