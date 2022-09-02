d7f58fb261c281f6c3425645001601c065616486
fatimah06 <fatima.m.mutairi@gmail.com>
2022-09-02 22:06:41 +0300

    js

--- challenge.js
+++ challenge.js
@@ -193,9 +193,3 @@ let names =  (...nam) =>{ return `String [${nam.join("] , [")}] => Done!`}
 
 console.log(names("Fatimah", "Bassam", "Youssef"));
 
-let mynumber = [20, 50, 10, 60];
-let calc = (one, two, ...nums) => {
-
-}
-
-console.log()
