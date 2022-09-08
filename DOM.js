40fb5726c551ee1877510b4477a57d7074e04e2b
fatimah06 <fatima.m.mutairi@gmail.com>
2022-09-08 16:10:11 +0300

    js

--- DOM.js
+++ DOM.js
@@ -3,5 +3,5 @@ let myElements = document.querySelector(".js");
 
 // innerHTML فايدته يكتب في الاتش تي ام ال حتى لو تاق يكتبه ويسوي تاق له 
 console.log(myElements.innerHTML);
-// 
+// فايدته يكتب انه نص يتجاهل التاق ولا يكتبه يعامله على انه نص بس txtcontent
 console.log(myElements.textContent)
