2324dac21b52eb8e962ce652b31ebd075289fa80
fatimah06 <fatima.m.mutairi@gmail.com>
2022-09-08 16:08:34 +0300

    js

--- DOM.js
+++ DOM.js
@@ -1,4 +1,7 @@
 let myElements = document.querySelector(".js");
 
+
+// innerHTML فايدته يكتب في الاتش تي ام ال حتى لو تاق يكتبه ويسوي تاق له 
 console.log(myElements.innerHTML);
+// 
 console.log(myElements.textContent)
