ebadfe195c5a4dc31142911fed8135c2c05663ce
fatimah06 <fatima.m.mutairi@gmail.com>
2022-09-09 14:27:30 +0300

    js

--- DOM.js
+++ DOM.js
@@ -12,5 +12,11 @@ console.log(document.getElementsByTagName("p")[0].attributes); // يجيب كل 
 
 let myp = document.getElementsByTagName("p")[0];
 if(myp.hasAttribute("data-src")) {
-    
+    if(myp.getAttribute("data-src") === "") {
+        myp.removeAttribute("data-src");
+    } else{
+        myp.setAttribute("data-src", "New Value in data-src");
+    }
+}else {
+    console.log(`Not Found`);
 }
