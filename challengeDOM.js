54d91717b0136bdd9060b0408b7ab2210aa0b00b
fatimah06 <fatima.m.mutairi@gmail.com>
2022-09-13 18:04:43 +0300

    challenges

--- challengeDOM.js
+++ challengeDOM.js
@@ -1,3 +1,11 @@
 let myheader = document.createElement("header");
 myheader.className= "head";
+let mytitle = document.createElement("h1");
+let mylogo = document.createTextNode("FATIMAH");
+let menu = document.createElement("")
 
+mytitle.style.cssText = "color: blue; margin-left:0px; font-size:20px; display: flex";
+
+mytitle.appendChild(mylogo);
+myheader.appendChild(mytitle);
+document.body.appendChild(myheader);
