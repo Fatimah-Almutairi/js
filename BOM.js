e10847f09c330b6cd63762caa9f77194bf9176fb
fatimah06 <fatima.m.mutairi@gmail.com>
2022-09-15 23:49:13 +0300

    js

--- BOM.js
+++ BOM.js
@@ -3,3 +3,11 @@ this.alert("Hello");
 alert("Hello");
 
 let confirmMS = confirm("Are You Sure?");
+
+console.log(confirmMS);
+
+if( confirmMS === true){
+    console.log("Item deleted");
+}else{
+    console.log("Item Not Deleted");
+}
