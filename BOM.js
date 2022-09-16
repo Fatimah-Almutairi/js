d6ad7b73dbfad3670aa4df65cbb1457cec626f17
fatimah06 <fatima.m.mutairi@gmail.com>
2022-09-16 20:56:45 +0300

    js

--- BOM.js
+++ BOM.js
@@ -1,17 +1,37 @@
-window.alert("Hello");
-this.alert("Hello");
-alert("Hello");
+// window.alert("Hello");
+// this.alert("Hello");
+// alert("Hello");
 
-let confirmMS = confirm("Are You Sure?");
+// let confirmMS = confirm("Are You Sure?");
 
-console.log(confirmMS);
+// console.log(confirmMS);
 
-if( confirmMS === true){
-    console.log("Item deleted");
-}else{
-    console.log("Item Not Deleted");
+// if( confirmMS === true){
+//     console.log("Item deleted");
+// }else{
+//     console.log("Item Not Deleted");
+// }
+
+// let promptMs = prompt("Write Your Age?", "25");
+
+// console.log(promptMs);
+
+// ====================================== 104 setTimeOut
+function SayMsg(user, age) {
+    console.log(`Hello I am ${user} and I have ${age} years old`);
 }
 
-let promptMs = prompt("Write Your Age?", "25");
+setTimeout(SayMsg, 2000, "Fatimah", 26);
 
-console.log(promptMs)
+// clearTimeout
+let counter = setTimeout(SayHi, 2000);
+
+function SayHi() {
+    console.log("Hiii");
+}
+
+let btn1 = document.querySelector(".clear");
+
+btn1.onclick = () => {
+    clearTimeout(counter);  // يوقف الفنكشن اللي تطبع هاي 
+}
