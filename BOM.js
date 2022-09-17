4c30166cabb4bd4559b77b96b13bfe11079b1536
fatimah06 <fatima.m.mutairi@gmail.com>
2022-09-17 23:59:17 +0300

    js

--- BOM.js
+++ BOM.js
@@ -108,5 +108,15 @@ window.onscroll = () => {
     if (window.scrollY >= 500) {
         // console.log(`Scrolling Y value is ${window.scrollY}`);
         btn.style.display = "block";
+    }else{
+        btn.style.display = "none";
     }
+};
+
+btn.onclick = () => {
+    window.scrollTo ({
+        left: 0,
+        top: 0,
+        behavior: "smooth"
+    });
 }
