d78c15181c3a1828c223ddfb231a31a41fc1f6f2
fatimah06 <fatima.m.mutairi@gmail.com>
2022-09-25 20:57:14 +0300

    js

--- Destructuring.js
+++ Destructuring.js
@@ -1,1 +1,6 @@
+// ============================================ 116
 let myFriends = ["Ahmed", "Sayed", "Ali", ["Shady", "Amr", ["Mohammed", "Gamal"]]];
+
+let [a, b] = myFriends[3][0],[3][2][1];
+console.log(a);
+console.log(b)
