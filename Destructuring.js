39b8717b6ce83f0d92d26f836075dbcf021e9517
fatimah06 <fatima.m.mutairi@gmail.com>
2022-09-25 21:02:29 +0300

    js

--- Destructuring.js
+++ Destructuring.js
@@ -1,8 +1,9 @@
 // ============================================ 116
 let myFriends = ["Ahmed", "Sayed", "Ali", ["Shady", "Amr", ["Mohammed", "Gamal"]]];
 
-let [a, b] = myFriends;
-a = myFriends[3][0];
-b = myFriends[3][2][1];
+// let [a, b] = myFriends; // هذي السطور طريقتي
+// a = myFriends[3][0];
+// b = myFriends[3][2][1];
+let [, , ,[a, ,[, b]]] = myFriends;  // هو سوو الطريقه هذي
 console.log(a);
 console.log(b)
