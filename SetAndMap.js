cc2f031749112c12110c50cb844260ebec73a3bf
fatimah06 <fatima.m.mutairi@gmail.com>
2022-10-04 22:21:26 +0300

    js

--- SetAndMap.js
+++ SetAndMap.js
@@ -297,11 +297,11 @@ let MyArray = [10, 20, 30, 40, 50, "A", "B"];
 
 // myArray.copyWithin(3); // [10, 20, 30, 10, 20, 30, 40]
 
-// myArray.copyWithin(4, 6); // [10, 20, 30, 40, "B", "A", "B"]
+// MyArray.copyWithin(4, 6); // [10, 20, 30, 40, "B", "A", "B"]
 
 // myArray.copyWithin(4, -1); // [10, 20, 30, 40, "B", "A", "B"]
 
 // myArray.copyWithin(1, -2); // [10, "A", "B", 40, 50, "A", "B"]
 
-MyArray.copyWithin(1, -2, -1);
+MyArray.copyWithin(1, -2, -1);  // [10, 'A', 30, 40, 50, 'A', 'B']
 console.log(MyArray);
