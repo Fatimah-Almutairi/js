6bf3d48cf2c03d07d43b40202cb42c22c9cabd8d
fatimah06 <fatima.m.mutairi@gmail.com>
2022-10-08 13:10:32 +0300

    js

--- SetAndMap.js
+++ SetAndMap.js
@@ -445,3 +445,26 @@ let objTwo = {
 };
 
 console.log({ ...objOne, ...objTwo, e: 5 }); // هنا جمعت الاوبجكت الاثنين مره وحده عشان كذا استعملت سبيلت 
+
+// 
+
+/*
+  Map And Set + What You Learn => Challenge
+  Requirements
+  - You Cant Use Numbers Or True Or False
+  - Don't Use Array Indexes
+  - You Cant Use Loop
+  - You Cant Use Any Higher Order Function
+  - Only One Line Solution Inside Console
+  - If You Use Length => Then Only Time Only
+  Hints
+  - You Can Use * Operator Only In Calculation
+  - Set
+  - Spread Operator
+  - Math Object Methods
+*/
+
+let n1 = [10, 30, 10, 20];
+let n2 = [30, 20, 10];
+
+console.log("Your Solution Here"); // 210
