0d0926bdbe5eaa38002893d65d6753fee92795ac
fatimah06 <fatima.m.mutairi@gmail.com>
2022-10-11 22:33:56 +0300

    js

--- Regular.js
+++ Regular.js
@@ -25,3 +25,5 @@ let myString = "Hello Elzero Web School I Love elzero";
 let regex = /elzero/ig;
 
 console.log(myString.match(regex));
+
+// =================================================================  136
